75e19a8e35104d210031310daa9a0f57354845bd
hika019 <ikazuti2001@gmail.com>
2019-02-09 22:04:17 +0900

    update file

--- LED/LED_Number.py
+++ LED/LED_Number.py
@@ -5,7 +5,9 @@ import RPi.GPIO as GPIO
 import time
 
 chanel = [3,5,7,8,10,11,12,13]
-times = 2
+times = 0.8
+counter =-1
+count = 15 +1
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(chanel, GPIO.OUT) 
@@ -13,25 +15,56 @@ GPIO.setup(chanel, GPIO.OUT)
 
 def clean():
     GPIO.output(chanel, GPIO.LOW)
+   
     
-def gpio():
+def pin(counter):
+    zero = [5,8,10,11,12,13]
     one = [10,11]
     two = [3,8,11,12,13]
     three = [3,8,10,11,13]
     four = [3,5,10,11]
     five = [3,5,8,10,13]
     six = [3,5,8,10,12,13]
-    return six
+    seven = [10,11,13]
+    eight = [3,5,8,10,11,12,13]
+    nign = [3,5,8,10,11,13]
+    
+    if counter == 0:
+        return zero
+    elif counter == 1:
+        return one
+    elif counter ==2:
+        return two
+    elif counter == 3:
+        return three
+    elif counter ==4:
+        return four
+    elif counter == 5:
+        return five
+    elif counter == 6:
+        return six
+    elif counter == 7:
+        return seven
+    elif counter == 8:
+        return eight
+    elif counter == 9:
+        return nign
 
-def HIGH(times):
-    chanels = gpio()
-    GPIO.output(chanels, GPIO.HIGH)
-    time.sleep(times)
-    clean()
+
+def HIGH(times, count):
+    global counter
+    for i in range(count):
+        counter = counter + 1
+        chanels = pin(counter)
+        GPIO.output(chanels, GPIO.HIGH)
+        time.sleep(times)
+        clean()
+        if counter == 9:
+            counter =-1
 
 
 
-HIGH(times)
+HIGH(times,count)
 
-time.sleep(2)
+time.sleep(0.2)
 GPIO.cleanup
